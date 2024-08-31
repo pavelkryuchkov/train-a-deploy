c7226ba1c02342e697227d262e5e742e82e5ab69
Pavel Kriuchkov <pavkr92@gmail.com>
2024-08-31 23:42:18 +0300

    Auto-generated commit

---
+++ chunk-DEIM6JNK.js
@@ -0,0 +1,1 @@
+function e(t){return r=>{let{value:n}=r;return n&&n.trim().length<t?{minlength:!0}:null}}export{e as a};
---
+++ chunk-MY2BAZRV.js
@@ -0,0 +1,1 @@
+var t=/^[\w\d_]+@[\w\d_]+\.\w{2,7}$/,o="manager",e=function(n){return n.invalidUniqueKey="invalidUniqueKey",n.rideNotFound="rideNotFound",n}(e||{});export{t as a,o as b,e as c};
