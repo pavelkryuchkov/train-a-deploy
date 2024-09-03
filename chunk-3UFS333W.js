6a7286d7b11db2fa47827959356edf43128b83a8
Pavel Kriuchkov <pavkr92@gmail.com>
2024-09-03 11:04:43 +0300

    Auto-generated commit

---
+++ chunk-3UFS333W.js
@@ -0,0 +1,1 @@
+var o=/^[\w\d_]+@[\w\d_]+\.\w{2,7}$/,n="manager",d=function(e){return e.invalidUniqueKey="invalidUniqueKey",e.rideNotFound="rideNotFound",e.alreadyBooked="alreadyBooked",e}(d||{});export{o as a,n as b,d as c};
